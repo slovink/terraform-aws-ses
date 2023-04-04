b1f3fcc894ac5ef7d35896115a6118efabff2cea
therahul28 <therahulyadav0077@gmail.com>
2023-04-04 15:36:44 +0530

    first commit

---
+++ _example/example.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "eu-west-1"
+}
+
+module "ses" {
+  source = "./../"
+
+  name        = "ses"
+  environment = "example"
+  label_order = ["name", "environment"]
+
+  domain   = ""
+  iam_name = "ses-user1"
+
+  enable_verification = false
+  enable_mx           = false
+  enable_spf_domain   = false
+}
